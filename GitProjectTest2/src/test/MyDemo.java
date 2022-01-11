617c12dec17a05714b1641ac0f182587999b9518
alexostr <alexostr1978@yahoo.com>
2022-01-10 20:30:19 -0500

    New method callME() is created

--- GitProjectTest2/src/test/MyDemo.java
+++ GitProjectTest2/src/test/MyDemo.java
@@ -4,6 +4,11 @@ public class MyDemo {
 
 	public static void main(String[] args) {
 		System.out.println("Hello GIT!!!");
+		System.out.println(callMe());
+	}
+	
+	public static String callMe() {
+		return "Congratulation!";
 	}
 
 }
